711ca861b8eb6899272f2274994ab6e767ddcb75
ekkis <ekkis@8ec180ab-3179-4e4b-b41a-6d47cb6653f6>
2009-09-28 23:57:45 +0000

    minor corrections + a bug fix introduced in r2460
    git-svn-id: svn+ssh://arix.com/Proc/PID/File@2465 8ec180ab-3179-4e4b-b41a-6d47cb6653f6

--- File.pm
+++ File.pm
@@ -174,7 +174,7 @@ sub debug {
 	my $self = shift;
 	my $msg = shift || $_;
 
-	print "> Proc::PID::File - $msg$\ "
+	print "> Proc::PID::File - $msg\n"
 		if $self->{debug};
 	}
 
@@ -186,7 +186,7 @@ sub DESTROY {
     	}
     
     open(PID, $self->{path})
-		|| die qq/Cannot open pid file \"$self->{path}\": $!\n/
+		|| die qq/Cannot open pid file "$self->{path}": $!\n/
         ;
 	my ($pid) = <PID> =~ /^(\d+)/;
     close PID;
@@ -245,7 +245,7 @@ For help and thank you notes, e-mail the author directly.  To report a bug, subm
 
 The latest version of the tarball, RPM and SRPM may always be found at: F<http://perl.arix.com/>  Additionally the module is available from CPAN.
 
-=head1 LICENSE
+=head1 LICENCE
 
 This utility is free and distributed under GPL, the Gnu Public License.  A copy of this license was included in a file called LICENSE. If for some reason, this file was not included, please see F<http://www.gnu.org/licenses/> to obtain a copy of this license.
 
